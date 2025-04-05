383c0b608edaf4d752b65175364a993cdda7f2ed
Quynh Nguyen <mr.quynh@gmail.com>
2025-04-06 00:53:17 +0700

    (improv): configs

--- Http/Startup.cs
+++ Http/Startup.cs
@@ -143,33 +143,33 @@ namespace net.vieapps.Services.Files
 			var onOutgoingConnectionEstablished = new List<Action<object, WampSessionCreatedEventArgs>>();
 			if (System.Configuration.ConfigurationManager.GetSection(UtilityService.GetAppSetting("Section:Maps", "net.vieapps.services.files.http.maps")) is AppConfigurationSectionHandler config && config.Section.SelectNodes("map") is System.Xml.XmlNodeList maps)
 				maps.ToList()
-					.Select(info => (Path: info.Attributes["path"]?.Value?.ToLower()?.Trim(), Type: info.Attributes["type"]?.Value))
-					.Where(info => !string.IsNullOrEmpty(info.Path) && !string.IsNullOrEmpty(info.Type))
-					.Select(info =>
+				.Select(info => (Path: info.Attributes["path"]?.Value?.ToLower()?.Trim(), Type: info.Attributes["type"]?.Value))
+				.Where(info => !string.IsNullOrEmpty(info.Path) && !string.IsNullOrEmpty(info.Type))
+				.Select(info =>
+				{
+					var path = info.Path;
+					while (path.StartsWith("/"))
+						path = path.Right(path.Length - 1);
+					while (path.EndsWith("/"))
+						path = path.Left(path.Length - 1);
+					return (Path: path, info.Type);
+				})
+				.Where(info => !Handler.Handlers.ContainsKey(info.Path))
+				.ForEach(info =>
+				{
+					try
 					{
-						var path = info.Path;
-						while (path.StartsWith("/"))
-							path = path.Right(path.Length - 1);
-						while (path.EndsWith("/"))
-							path = path.Left(path.Length - 1);
-						return (Path: path, info.Type);
-					})
-					.Where(info => !Handler.Handlers.ContainsKey(info.Path))
-					.ForEach(info =>
+						if (AssemblyLoader.GetType(info.Type)?.CreateInstance() is PathMapper mapper)
+						{
+							appBuilder.Map($"/{info.Path}", builder => mapper.Map(builder, appLifetime, onIncomingConnectionEstablished, onOutgoingConnectionEstablished));
+							Global.Logger.LogInformation($"Successfully branch the request to a specified path: /{info.Path} => {mapper.GetTypeName()}");
+						}
+					}
+					catch (Exception ex)
 					{
-						try
-						{
-							if (AssemblyLoader.GetType(info.Type)?.CreateInstance() is PathMapper mapper)
-							{
-								appBuilder.Map($"/{info.Path}", builder => mapper.Map(builder, appLifetime, onIncomingConnectionEstablished, onOutgoingConnectionEstablished));
-								Global.Logger.LogInformation($"Successfully branch the request to a specified path: /{info.Path} => {mapper.GetTypeName()}");
-							}
-						}
-						catch (Exception ex)
-						{
-							Global.Logger.LogError($"Cannot load a path mapper ({info.Type}) => {ex.Message}", ex);
-						}
-					});
+						Global.Logger.LogError($"Cannot load a path mapper ({info.Type}) => {ex.Message}", ex);
+					}
+				});
 
 			// setup the handler for all requests
 			appBuilder.UseMiddleware<Handler>();
